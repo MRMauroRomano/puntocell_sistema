74092ab2a5048b085246452cb02556bde15089b7
Mauro Romano <mauroromano09@gmail.com>
2026-02-12 14:23:01 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2933333639-3967d",
+  "appId": "1:971384440879:web:9fc332d02a52db96479eda",
+  "apiKey": "AIzaSyDZE-ptKuhiP6Mwu9Esr9frZRsNtY0gVU4",
+  "authDomain": "studio-2933333639-3967d.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "971384440879"
+};
